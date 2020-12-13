25a1c361339cbcfcad7048913b40eaed276412a4
kinist <kinist@msn.com>
2020-12-13 22:00:50 +0800

    升级Jenkins docker-compose脚本

--- jenkins/slave/agent/slave.sh
+++ jenkins/slave/agent/slave.sh
@@ -1,18 +1,37 @@
 #!/bin/sh
 
-CUR_DIR=$(pwd)
-#JVM_OPTIONS="-Xms2G -Xmx4G"
+# Shell scripts for Jenkins slave start stop status.
+# Author: Gavin Zhang
+
+# Slave APP Jar executable file
 APP_NAME=agent.jar
-SECRET_KEY=50a5d0445eee59aaf9f99908da1e66d92a11cba4d8e340513fed8e170b8e9467
+
+# Conection secret
+SECRET_KEY=5eabed57fef34abc6856d96c2a3bea841bea10bef62e202ef64e156a76783113
+
+# Remote root directory
 WORK_DIR=/opt/jenkins/slave/nodes
-JNLP_URL=http://127.0.0.1:8080/computer/slave_agent/slave-agent.jnlp
+
+# Jenkins master
+JNLP_URL=http://192.168.128.128:8080/computer/slave_node/slave-agent.jnlp
+
+# Jenkins use https:0 ; Jenkins use http:1
+HTTPS_TYPE=1
+
+# JVM Options
+#JVM_OPTIONS="-Xms2G -Xmx4G"
 
 start()
 {
+    CUR_DIR=$(pwd)
     PID=$(ps -ef | grep -w "$APP_NAME" | grep -v "grep" | awk '{print $2}')
     if [ -z $PID ]; then
         if [ -f $CUR_DIR/$APP_NAME ]; then
-            nohup java -jar $JVM_OPTIONS $CUR_DIR/$APP_NAME -jnlpUrl $JNLP_URL -secret $SECRET_KEY -workDir $WORK_DIR &
+            if [ $HTTPS_TYPE -eq 0 ]; then
+                nohup java -jar $JVM_OPTIONS $CUR_DIR/$APP_NAME -jnlpUrl $JNLP_URL -noCertificateCheck -secret $SECRET_KEY -workDir $WORK_DIR &
+            else
+                nohup java -jar $JVM_OPTIONS $CUR_DIR/$APP_NAME -jnlpUrl $JNLP_URL -secret $SECRET_KEY -workDir $WORK_DIR &
+            fi
             sleep 1
             echo "Starting Jenkins salve......      [  OK  ]"
         else
@@ -37,26 +56,26 @@ stop()
 
 case "$1" in
     start)
-	start
-	;;
+        start
+        ;;
     stop)
-	stop
-	;;
+        stop
+        ;;
     restart)
         stop
         start
         ;;
     status)
         PID=$(ps -ef | grep -w "$APP_NAME" | grep -v "grep" | awk '{print $2}')
-	if [ "$PID" == "" ]; then
+        if [ "$PID" == "" ]; then
             echo "Jenkins slave is stopped."
         else
             echo "Jenkins slave is running, PID="$PID"."
         fi
-		;;
-	*)
+        ;;
+    *)
 	echo $"Usage: $0 {start|stop|restart|status}"
-	RETVAL=1
+        RETVAL=1
 esac
 exit $RETVAL
 
